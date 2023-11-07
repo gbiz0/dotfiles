061084757c3cdf045937f7d4199e415655a100e0
gbiz0 <jardimbgustavo@gmail.com>
2023-11-07 10:06:20 -0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+# file: install.sh
+# author: Gustavo Bizo Jardim <gustavobizo@protonmail.com>
+# date: Nov 7, 2023
+
+# check os
+os=$(uname)
+if [[ ${os} != 'Linux' ]]
+then
+  echo '${os} not supported.'
+
+  exit 0
+fi
+
+dotfiles_dir=$(pwd)
+ 
+echo 'Installing base'
+
+# install base
+sudo apt-get update
+
+#dev tools
+sudo apt-get install -y \
+  build-essential \
+  curl \
+  git \
+  htop \
+  jq \
+  neovim \
+  python3 \
+  python3-pip \
+  python3-venv \
+  tmux \
+  tree \
+  wget \
+  zsh \
+  vscode \
+  python3-neovim
+
+#tools 
+sudo apt install obs-studio -y \
+ discord \
+
+#nvchad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+#sudo apt-get install gcc-mingw-w64
+
